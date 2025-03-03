3e6ca66fe18a372ec5aac0f11bbf8eb5b24725db
Joshua Ability <joshua4ability@gmail.com>
2025-03-03 22:48:30 +0100

    Medical Chat Bot

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chat Bot',
+    version= '0.0.0',
+    author= 'Joshua Ability',
+    author_email= 'mrjoshuaability@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
